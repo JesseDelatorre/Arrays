5a82db20e055450ac8cd8d338d7e4e6599c48744
Jesse <Existance@jesses-MacBook-Pro.local>
2025-01-22 08:44:29 -0500

    added js code

--- index.js
+++ index.js
@@ -12,3 +12,66 @@
 //getEvens
 //getOdds
 
+
+
+const userInput = prompt(`Enter some numbers seperated by commas`);
+const userNumbers = userInput.split(`,`);
+const numbers = [];
+for(let indexCounter = 0; indexCounter < userNumbers.length; indexCounter = indexCounter + 1){
+const userNumberAsString = userNumbers[indexCounter];
+const userNumberAsNumber = Number(userNumberAsString)
+numbers.push(userNumberAsNumber);
+//console.log(userNumberAsNumber);
+}
+const getLength = (numbers) => numbers.length;
+alert(`your length is ` + getLength(numbers));
+
+const getSum = (numbers) => {
+  let sum = 0
+  for(let i=0; i< numbers.length; i++){
+sum += numbers[i];
+  }
+  return sum
+}
+console.log(getSum(numbers));
+
+const getMean = (numbers) => {
+  return getSum(numbers) / getLength(numbers);
+}
+console.log(getMean(numbers));
+
+const getMin = (numbers) => {
+let smallest
+for(let i=0; i<numbers.length; i++){
+  if( i=== 0 || smallest > numbers[i]){
+    smallest = numbers[i]
+  }
+}
+return smallest
+}
+//console.log(getMin(numbers));
+
+const getMax = (numbers) => {
+  let largest
+  for(let i=0; i<numbers.length; i++){
+    if( i=== 0 || largest > numbers[i]){
+      largest = numbers[i]
+    }
+  }
+  return largest
+  }
+  console.log(getMax(numbers));
+
+
+const getEvens = (numbers) => {
+  let evens = [];
+for(let i=0; i<numbers.length; i++){
+  if(number[i] % 2 === 0){
+  
+  }
+}
+return evens
+}
+
+//  const get___  = () => {
+//  } 
