97e681ce71000a147a2fdad51055b2ca33808a6e
Jesse <Existance@jesses-MacBook-Pro.local>
2025-01-22 08:43:13 -0500

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+// create a prompt that gets numbers seperated by commas
+// create an empty array
+// take userInput and turn it into array
+// convert strings to numbers
+// create functions that will be passed in the array
+// getLength
+//getSum
+//getMean
+//getMin
+//getMax
+//getRange
+//getEvens
+//getOdds
+
